0f4f4436a6b791d966c67cca3092413260fe6842
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-01-08 18:54:16 +0100

    created new mixins and updated form ui

--- set-a/react-comp-lib/src/components/formComp.js
+++ set-a/react-comp-lib/src/components/formComp.js
@@ -1,20 +1,101 @@
-import '../assets/css/styles.css';
+import "../assets/scss/mixins/_form.scss";
 
-function FormComp(){
+function FormComp() {
+  return (
+    <div className="form-container">
+      <h3>Payment Information</h3>
+      <br />
+      <div >
+        <form action="">
+          <div>
+            <label for="paymethod" className="form-label">Select Payment Method</label>
+            <input
+              type="text"
+              id="paymethod"
+              name="firstname"
+              placeholder="Select Payment Method"
+            />
+          </div>
 
-    return (
-        <div >
-            <h3>Payment Information</h3>
-            <br />
-            <form>
-                <div className='inline-input'>
-                    <label for="PayMethod" >Select Payment Method</label><br />
-                    <input type="text" />
-                </div>
-            </form>
-        </div>
-    )
+          <div>
+            <label for="cardnumber" className="form-label">Card Number</label>
+            <input
+              type="text"
+              id="cardnumber"
+              name="cardnumber"
+              placeholder="0000-0000-0000-0000"
+            />
+          </div>
+          <div className="inline-input">
+            <div className="column-input">
+              <label for="expdate" className="form-label">Expiry Date</label>
+              <div className="inline-input">
+                <input
+                  type="text"
+                  id="expdate"
+                  name="expdate"
+                  placeholder="MM"
+                  style={{marginRight:"15px"}}
+                />
+                <input
+                  type="text"
+                  id="expdate"
+                  name="expdate"
+                  placeholder="YYYY"
+                />
+              </div>
+            </div>
+            <div className="column-input">
+              <label for="cvv" className="form-label">Cvv</label>
+              <input
+                type="password"
+                id="cvv"
+                name="cvv"
+                placeholder="cvv"
+              />
+            </div>
+          </div>
 
+          <div>
+            <label for="cardholder" className="form-label">Card Holder's name</label>
+            <input
+              type="text"
+              id="cardholder"
+              name="cardholder"
+              placeholder="card holder's name.."
+            />
+          </div>
+          <div className="inline-input">
+            <div className="column-input">
+              <label for="postalcode" className="form-label">Postal Code</label>
+              <div className="inline-input">
+                <input
+                  type="text"
+                  id="postalcode"
+                  name="postalcode"
+                  placeholder="Postal Code"
+                />
+              </div>
+            </div>
+            <div className="column-input">
+              <label for="country"className="form-label">Country</label>
+              <input
+                type="text"
+                id="country"
+                name="country"
+                placeholder="Country"
+              />
+            </div>
+            
+           
+          </div>
+          <div >
+            <button className="form-button">Continue to Confirmation</button>
+            </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default FormComp;
