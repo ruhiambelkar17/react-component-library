8e85940bbe742f9d74d379b006ebe6d7d391565f
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-01-11 23:16:21 +0100

    replaced all inputs with common input component

--- set-a/react-comp-lib/src/components/formComp.js
+++ set-a/react-comp-lib/src/components/formComp.js
@@ -3,56 +3,69 @@ import ButtonComp from "./buttonComp";
 import InputComp from "./inputComp";
 
 function FormComp() {
+    const mr={marginRight:'25px'};
   return (
     <div className="form-container">
       <h3>Payment Information</h3>
       <br />
-      <div >
+      <div>
         <form action="">
           <div>
-            <label for="paymethod" className="form-label">Select Payment Method</label>
-            <div style={{display:"inline-flex",width:"100%"}}>
-                <div className="form-input" style={{marginRight:"25px"}}>
-                    <input id="visa" type="radio" />
-                    <label className="form-label" for="visa">Visa</label>
-                </div>
-                
-                <div className="form-input"style ={{marginRight:"25px"}} >
-                    <input type="radio" id="Mastercard" />
-                    <label className="form-label" for="Mastercard">Mastercard</label>
-                </div>
-                <div className="form-input"  >
-                    <input type="radio" id="PayPal" />
-                    <label className="form-label" for="PayPal" >PayPal</label>
-                </div>
+            <label for="paymethod" className="form-label">
+              Select Payment Method
+            </label>
+            <div style={{ display: "inline-flex", width: "100%" }}>
+              <div className="form-input" style={{ marginRight: "25px" }}>
+                <input id="visa" type="radio" />
+                <label className="form-label" for="visa">
+                  Visa
+                </label>
+              </div>
+
+              <div className="form-input" style={{ marginRight: "25px" }}>
+                <input type="radio" id="Mastercard" />
+                <label className="form-label" for="Mastercard">
+                  Mastercard
+                </label>
+              </div>
+              <div className="form-input">
+                <input type="radio" id="PayPal" />
+                <label className="form-label" for="PayPal">
+                  PayPal
+                </label>
+              </div>
             </div>
           </div>
 
           <div>
-            
-            <InputComp 
-                for="cardnumber"
-                label="Card Number" 
-                type="text"
-                id="cardnumber"
-                name="cardnumber"
-                placeholder="0000-0000-0000-0000"
+            <InputComp
+              for="cardnumber"
+              label="Card Number"
+              type="text"
+              id="cardnumber"
+              name="cardnumber"
+              placeholder="0000-0000-0000-0000"
             ></InputComp>
           </div>
           <div className="inline-input">
             <div className="column-input">
-              <label for="expdate" className="form-label">Expiry Date</label>
               <div className="inline-input">
-                <input
-                className="form-input"
+                <div style={{marginRight:"15px"}}>
+                <InputComp
+                  for="expdate"
+                  label="Expiry Date"
+                
                   type="text"
                   id="expdate"
                   name="expdate"
                   placeholder="MM"
-                  style={{marginRight:"15px"}}
+                  
                 />
-                <input
-                className="form-input"
+                </div>
+                <InputComp
+                  for="expdate"
+                  label="Expiry Date"
+                  
                   type="text"
                   id="expdate"
                   name="expdate"
@@ -60,23 +73,27 @@ function FormComp() {
                 />
               </div>
             </div>
-            <div className="column-input" style={{display:"flex",justifyContent:"end"}}>
-              <label for="cvv" className="form-label">Cvv</label>
-              <input
-              className="form-input"
+            <div
+              className="column-input"
+              style={{ display: "flex", justifyContent: "end" }}
+            >
+              <InputComp
+                for="cvv"
+                label="Cvv"
+               
                 type="password"
                 id="cvv"
                 name="cvv"
                 placeholder="cvv"
-
               />
             </div>
           </div>
 
           <div>
-            <label for="cardholder" className="form-label">Card Holder's name</label>
-            <input
-            className="form-input"
+            <InputComp
+              for="cardholder"
+              label="Card Holder's name"
+              
               type="text"
               id="cardholder"
               name="cardholder"
@@ -85,29 +102,28 @@ function FormComp() {
           </div>
           <div className="inline-input">
             <div className="column-input">
-              <label for="postalcode" className="form-label">Postal Code</label>
-              <div className="inline-input">
-                <input
-                className="form-input"
+              
+                <InputComp
+                  for="postalcode"
+                  label="Postal Code"
+                  
                   type="text"
                   id="postalcode"
                   name="postalcode"
                   placeholder="Postal Code"
                 />
-              </div>
+              
             </div>
             <div className="column-input">
-              <label for="country"className="form-label">Country</label>
-              <input
-              className="form-input"
+              <InputComp
+                for="country"
+                label="Country"
                 type="text"
                 id="country"
                 name="country"
                 placeholder="Country"
               />
             </div>
-            
-           
           </div>
           <ButtonComp>Continue to Confirmation</ButtonComp>
         </form>
--- set-a/react-comp-lib/src/components/inputComp.js
+++ set-a/react-comp-lib/src/components/inputComp.js
@@ -10,6 +10,7 @@ export default function InputComp(props){
               id={props.id}
               name={props.name}
               placeholder={props.placeholder}
+              
             />
         </div>
     )
